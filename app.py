74a5bf147cce578c8258d201e787ed9082e56fa9
OnurYilmazGit <46044317+OnurYilmazGit@users.noreply.github.com>
2023-06-05 22:40:05 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_file
+
+app = Flask(__name__, static_folder='.')
+
+@app.route('/')
+def index():
+    return send_file('decision_tree.html')
+
+if __name__ == "__main__":
+    app.run(port=8080)
