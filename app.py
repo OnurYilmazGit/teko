e9551043e8f248bcb6eca1001feadbfb28385724
EricOfficial <eric.naeser@gmx.de>
2023-06-06 08:51:52 +0200

    added requirements.txt and OpenAI APIs refactored scripts and pages in corresponding folders

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def index():
-    return send_file('decision_tree.html')
+    return send_file('./pages/decision_tree.html')
 
 if __name__ == "__main__":
     app.run(port=8080)
---
+++ src/open_ai_api.py
@@ -0,0 +1,49 @@
+from decouple import config
+import openai
+
+
+def rephrase_description(situation_description):
+    openai_api_key = config("OPENAI_API")
+    openai.api_key = openai_api_key
+
+    instruction = f"Reformulate the following text to a natural-sounding and accurate text for legal documents.\n"
+    prompt = instruction + situation_description
+    print(prompt)
+
+    completions = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].message.content
+    print(" ")
+    print(message)
+    return message
+
+
+def receive_correct_court(type, city):
+    openai_api_key = config("OPENAI_API")
+    openai.api_key = openai_api_key
+
+    prompt = f"I am living in city corresponding to PLZ {city}. At which {type} do I have to file a case for tenancy law? Please give me as an answer only the name of the court."
+    print(prompt)
+
+    completions = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].message.content
+    print(" ")
+    print(message)
+    return message
+
+
+desc = "The crime happened at May 5, 2023. My brother and I were walking down the street. A man in a suit came from the left and destroyed the car's window."
+rephrase_description(desc)
