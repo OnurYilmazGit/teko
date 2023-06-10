5434d1100ab2929c33e7fc417837af048a41f680
OnurYilmazGit <46044317+OnurYilmazGit@users.noreply.github.com>
2023-06-10 09:25:52 +0200

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -11,7 +11,46 @@ from pages.custom_components import get_local_pages, show_navbar, chapter_spacer
 # Set config
 st.set_page_config(initial_sidebar_state="collapsed", layout="wide")
 hide_pages(get_local_pages())
-current_path = str(Path(__file__).parents[0])
+# Current path
+current_path = str(Path(__file__).parents[1])
+
+# Define about us information
+people_info = {
+    "Person 1": {
+        "image_path": f"{current_path}/assets/house.jpg",
+        "description": "Description for Person 1."
+    },
+    "Person 2": {
+        "image_path": f"{current_path}/assets/house.jpg",
+        "description": "Description for Person 2."
+    },
+    "Person 3": {
+        "image_path": f"{current_path}/assets/house.jpg",
+        "description": "Description for Person 3."
+    },
+    "Person 4": {
+        "image_path": f"{current_path}/assets/house.jpg",
+        "description": "Description for Person 4."
+    },
+}
+
+# Title
+st.title("About Us")
+
+# Loop over people
+for person, info in people_info.items():
+    # Create two columns
+    col1, col2 = st.columns(2)
+
+    # First column: image
+    with col1:
+        image = Image.open(info["image_path"])
+        st.image(image, width=int(image.size[0]*0.125))
+
+    # Second column: info
+    with col2:
+        st.header(person)
+        st.write(info["description"])
 
 
 # Create Navbar
