73365fe02d395fb57418ea878dbe8d7865ef1e31
OnurYilmazGit <46044317+OnurYilmazGit@users.noreply.github.com>
2023-06-11 21:37:35 +0200

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -1,57 +1,47 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
-from streamlit_extras import card
-from st_pages import hide_pages, Page, show_pages
 from PIL import Image
 from pathlib import Path
-from streamlit_option_menu import option_menu
 from pages.custom_components import get_local_pages, show_navbar, chapter_spacer, components_spacer
-
-
-# Set config
-st.set_page_config(initial_sidebar_state="collapsed", layout="wide")
-hide_pages(get_local_pages())
 # Current path
 current_path = str(Path(__file__).parents[1])
 
-# Define about us information
-people_info = {
-    "Person 1": {
-        "image_path": f"{current_path}/assets/house.jpg",
-        "description": "Description for Person 1."
+# List of profiles
+profiles = [
+    {
+        "name": "Thomas",
+        "image": f"{current_path}/assets/Thomas.jpeg",
+        "description": "Thomas is a software engineer with a passion for AI."
     },
-    "Person 2": {
-        "image_path": f"{current_path}/assets/house.jpg",
-        "description": "Description for Person 2."
+    {
+        "name": "Onur",
+        "image": f"{current_path}/assets/Onur.jpg",
+        "description": "Onur is a data scientist who loves uncovering insights from data."
     },
-    "Person 3": {
-        "image_path": f"{current_path}/assets/house.jpg",
-        "description": "Description for Person 3."
+    {
+        "name": "Eric",
+        "image": f"{current_path}/assets/Eric.png",
+        "description": "Eric is a software engineer with a passion for AI."
     },
-    "Person 4": {
-        "image_path": f"{current_path}/assets/house.jpg",
-        "description": "Description for Person 4."
-    },
-}
+    {
+        "name": "Klare",
+        "image": f"{current_path}/assets/Klare.png",
+        "description": "Klare is a data scientist who loves uncovering insights from data."
+    },  
+    
+]
 
-# Title
-st.title("About Us")
+# Display title
+st.title('Who We Are')
 
-# Loop over people
-for person, info in people_info.items():
-    # Create two columns
-    col1, col2 = st.columns(2)
+# Preprocess images to make them the same size
+image_size = (300, 300)  # Set the size you want
 
-    # First column: image
-    with col1:
-        image = Image.open(info["image_path"])
-        st.image(image, width=int(image.size[0]*0.125))
-
-    # Second column: info
-    with col2:
-        st.header(person)
-        st.write(info["description"])
-
-
-# Create Navbar
-show_navbar()
+# Display each profile in a grid
+cols = st.columns(2)  # Adjust the number based on how many columns you want
+for i, profile in enumerate(profiles):
+    with cols[i % 2]:  # Adjust the modulus based on how many columns you have
+        img = Image.open(profile['image'])
+        img = img.resize(image_size)
+        st.image(img)
+        st.markdown(f"**{profile['name']}**")
+        st.markdown(profile['description'])
