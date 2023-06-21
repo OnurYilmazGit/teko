fa928773c6513f4d455e84a60aedaa3672ffa286
thomas <thomas.se.wagner@tum.de>
2023-06-21 10:19:32 +0200

    evidence improvements

--- pages/question_evidence.py
+++ pages/question_evidence.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from st_pages import hide_pages, Page, show_pages
 from PIL import Image
+import re
 from pathlib import Path
 from pages.custom_components import *
 
@@ -23,18 +24,37 @@ for attr in list(question_steps.keys()) + list(document_steps.keys()):
 show_navbar()
 show_sidebar()
 
+address = st.session_state.question_plaintiff
+
+# Split the string into lines
+lines = address.split('\n')
+
+# Get the street and city
+street = lines[1].strip()
+street = street.replace(',', '')
+zipcode_city = lines[2].strip()
+
+# Create new string in the desired format
+new_address = f"{street} in {zipcode_city}"
+
+
 
 chapter_spacer()
 st.subheader(f"Please select relevant exemplary evidence or provide your own")
 st.progress((1.0 / 7) * current_step)
 
-evidence_dict = {"Evidence 1": "Desc 1\n", "Evidence 2": "Desc 2\n", "Evidence 3": "Desc 3\n"}
+evidence_dict = {"Mietvertrag": "Der Kläger begehrt Rückzahlung einer Mietkaution nach beendetem Mietvertrag. Mit Mietvertrag vom dd.mm.yyyy mietete der Kläger vom Beklagten die Mietwohnung in der "
+                 + new_address + " (im Folgenden „Wohnung“).\n\n",
+                 "Evidence 2": "Desc 2\n\n", "Evidence 3": "Desc 3\n\n"}
 
 selected_evidence = st.multiselect(label="", options=evidence_dict.keys())
 
+cnt = 1
 desc_string = ""
 for selection in selected_evidence:
-    desc_string += evidence_dict[selection]
+    desc_string += evidence_dict[selection] 
+    desc_string += "Beweis: Anlage K " + str(cnt) + " - " + selection + " vom dd.mm.yyyy (Kopie)\n\n"
+    cnt += 1
 
 question_evidence = st.text_area(label="label", value=desc_string, height=400, label_visibility="hidden").strip()
 
